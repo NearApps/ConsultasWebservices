a2a232fbcf6343a956127b5b7bfa04fed0732e14
J. Carlos <jc_01_95@hotmail.com>
2021-06-20 12:06:15 -0500

    visualizar resultados

--- app/src/main/java/com/near/consultadnis/Principal.java
+++ app/src/main/java/com/near/consultadnis/Principal.java
@@ -8,14 +8,18 @@ import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.blankj.utilcode.util.LogUtils;
+import com.blankj.utilcode.util.NetworkUtils;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.near.consultadnis.database.Datos;
 import com.near.consultadnis.database.entidades.Personas;
 import com.near.consultadnis.utils.Utils;
+
+import org.json.JSONObject;
 
 import cz.msebera.android.httpclient.Header;
 
@@ -25,7 +29,7 @@ public class Principal extends AppCompatActivity {
 
     private EditText txt_search;
     private Button btn_search;
-    private TextView lbl_result;
+    private LinearLayout ly_result;
 
     private ProgressDialog progressDialog;
 
@@ -38,7 +42,7 @@ public class Principal extends AppCompatActivity {
 
         txt_search = (EditText) findViewById(R.id.txt_search);
         btn_search = (Button) findViewById(R.id.btn_search);
-        lbl_result = (TextView) findViewById(R.id.lbl_result);
+        ly_result = (LinearLayout) findViewById(R.id.ly_result);
 
         progressDialog = new ProgressDialog(Principal.this);
         progressDialog.setIndeterminate(true);
@@ -48,53 +52,80 @@ public class Principal extends AppCompatActivity {
         btn_search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if ( !TextUtils.isEmpty( txt_search.getText().toString() ) ){
-                    if ( txt_search.getText().toString().length()==8  ){
 
-                        progressDialog.show();
-                        v.setEnabled( false );
+                ly_result.removeAllViews();
 
-                        try{
-                            AsyncHttpClient cliente = new AsyncHttpClient();
-                            cliente.get( (Utils.var.WS + txt_search.getText().toString()), null, new AsyncHttpResponseHandler() {
-                                @Override
-                                public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
-                                    if ( statusCode == 200 ){
-                                        try {
-                                            LogUtils.d( (new String( responseBody )) );
-                                            if ( !TextUtils.isEmpty( (new String( responseBody )) ) ){
-                                                lbl_result.setText( (new String( responseBody )) );
+                if (!TextUtils.isEmpty( txt_search.getText().toString() )){
+                    if( sqlite.tb_personas().find( txt_search.getText().toString() ) == null ){
+                        if ( NetworkUtils.isConnected() ){
+                            if ( txt_search.getText().toString().length()==8  ){
+
+                                progressDialog.show();
+                                v.setEnabled( false );
+
+                                try{
+                                    AsyncHttpClient cliente = new AsyncHttpClient();
+                                    cliente.get( (Utils.var.WS + txt_search.getText().toString()), null, new AsyncHttpResponseHandler() {
+                                        @Override
+                                        public void onSuccess(int statusCode, Header[] headers, byte[] responseBody) {
+                                            if ( statusCode == 200 ){
+                                                try {
+                                                    LogUtils.d( (new String( responseBody )) );
+                                                    if ( !TextUtils.isEmpty( (new String( responseBody )) ) ){
+
+                                                        Personas.tb persona = processPerson( (new String( responseBody )) );
+                                                        if ( persona != null ){
+                                                            //Utils.function.showToastSuccess( "" );
+                                                            if ( sqlite.tb_personas().find( persona.getDni() ) == null ){
+                                                                sqlite.tb_personas().insert( persona );
+                                                            }else{
+                                                                sqlite.tb_personas().update( persona );
+                                                            }
+
+                                                            print_person( persona.getDni() );
+
+                                                        }else{
+                                                            Utils.function.showToastError( "No se hubo resutados" );
+                                                        }
+
+                                                    }
+                                                }catch (Exception ex){
+                                                    ex.printStackTrace();
+                                                }finally {
+                                                    if (progressDialog.isShowing()){
+                                                        progressDialog.cancel();
+                                                    }
+                                                }
                                             }
-                                        }catch (Exception ex){
-                                            ex.printStackTrace();
-                                        }finally {
+                                        }
+                                        @Override
+                                        public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
+                                            error.printStackTrace();
                                             if (progressDialog.isShowing()){
                                                 progressDialog.cancel();
                                             }
+                                            Utils.function.showToastError( "No hemos encontrado DNI" );
                                         }
-                                    }
-                                }
-                                @Override
-                                public void onFailure(int statusCode, Header[] headers, byte[] responseBody, Throwable error) {
-                                    error.printStackTrace();
-                                    if (progressDialog.isShowing()){
-                                        progressDialog.cancel();
-                                    }
+
+                                        @Override
+                                        public void onFinish() {
+                                            super.onFinish();
+                                        }
+                                    });
+                                }catch (Exception ex){
+                                    ex.printStackTrace();
+                                }finally {
+                                    v.setEnabled(true);
                                 }
 
-                                @Override
-                                public void onFinish() {
-                                    super.onFinish();
-                                }
-                            });
-                        }catch (Exception ex){
-                            ex.printStackTrace();
-                        }finally {
-                            v.setEnabled(true);
+                            }else{
+                                Utils.function.showToastError( "DNI no válido." );
+                            }
+                        }else{
+                            Utils.function.showToastError( "NO esta conectado a internet." );
                         }
-
                     }else{
-                        Utils.function.showToastError( "DNI no válido." );
+                        print_person( txt_search.getText().toString() );
                     }
                 }else{
                     Utils.function.showToastError( "Escriba un DNI." );
@@ -104,6 +135,82 @@ public class Principal extends AppCompatActivity {
 
 
     }
+
+
+    private Personas.tb processPerson( final String response ) {
+
+        try{
+            JSONObject jsonObject = new JSONObject( response );
+
+            if ( jsonObject.getString("success").equals("true")  ){
+                JSONObject jsonPersona = new JSONObject( jsonObject.getString("result") );
+
+                Personas.tb persona = new Personas.tb(
+                        jsonPersona.getString("num_doc"),
+                        jsonPersona.getString("verificacion"),
+                        jsonPersona.getString("nombres"),
+                        jsonPersona.getString("apellido_paterno"),
+                        jsonPersona.getString("apellido_materno"),
+                        jsonPersona.getString("fecha_nacimiento"),
+                        ( jsonPersona.has("edad") ? jsonPersona.getString("edad") : "" ),
+                        jsonPersona.getString("sexo"),
+                        jsonPersona.getString("ubi_dir_dist"),
+                        jsonPersona.getString("ubi_dir_prov"),
+                        jsonPersona.getString("ubi_dir_depa")
+                );
+                return persona;
+            }else{
+                return null;
+            }
+
+        }catch (Exception ex){
+            ex.printStackTrace();
+            return null;
+        }
+    }
+
+
+    private void print_person( final String dni ){
+        try{
+            final Personas.tb persona = sqlite.tb_personas().find( dni );
+            if ( persona!=null ){
+                ly_result.post(new Runnable() {
+                    @Override
+                    public void run() {
+                        ly_result.addView( build_item( "DNI", (persona.getDni() + "-" + persona.getCod_verificcion()) ) );
+                        ly_result.addView( build_item( "Nombre", persona.getNombre() ) );
+                        ly_result.addView( build_item( "Apellido Paterno", persona.getAp_paterno() ) );
+                        ly_result.addView( build_item( "Apellido Materno", persona.getAp_materno()) );
+                        ly_result.addView( build_item( "Fecha Nac", persona.getNacimiento() ) );
+                        ly_result.addView( build_item( "Edad", persona.getEdad() ) );
+                        ly_result.addView( build_item( "sexo", persona.getSexo() ) );
+                        ly_result.addView( build_item( "distrito", persona.getUbi_dist() ) );
+                        ly_result.addView( build_item( "Provincia", persona.getUbi_prov() ) );
+                        ly_result.addView( build_item( "departamento", persona.getUbi_depa() ) );
+                    }
+                });
+            }
+        }catch (Exception ex){
+            ex.printStackTrace();
+            ly_result.removeAllViews();
+        }
+    }
+    private LinearLayout build_item( final String titulo, final String result ){
+        LinearLayout ly_item_result = new LinearLayout( Principal.this );
+        try{
+            ly_item_result = (LinearLayout) View.inflate( Principal.this, R.layout._item_result, null );
+
+            final TextView txt_titulo = (TextView) ly_item_result.findViewById(R.id.txt_titulo);
+            txt_titulo.setText( titulo.toUpperCase() );
+
+            ((TextView) ly_item_result.findViewById(R.id.txt_result)).setText(result);
+
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return ly_item_result;
+    }
+
 
     @Override
     protected void onResume() {
--- app/src/main/java/com/near/consultadnis/database/entidades/Personas.java
+++ app/src/main/java/com/near/consultadnis/database/entidades/Personas.java
@@ -1,6 +1,7 @@
 package com.near.consultadnis.database.entidades;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.room.ColumnInfo;
 import androidx.room.Dao;
 import androidx.room.Entity;
@@ -17,21 +18,54 @@ public class Personas {
     public static class tb{
         @PrimaryKey
         @ColumnInfo(name = "dni") @NonNull              private String dni;
+        @ColumnInfo(name = "cod_verificcion") @Nullable private String cod_verificcion;
         @ColumnInfo(name = "nombre") @NonNull           private String nombre;
-        @ColumnInfo(name = "apellido") @NonNull         private String apellido;
+        @ColumnInfo(name = "ap_paterno") @NonNull       private String ap_paterno;
+        @ColumnInfo(name = "ap_materno") @NonNull       private String ap_materno;
+        @ColumnInfo(name = "nacimiento") @Nullable      private String nacimiento;
+        @ColumnInfo(name = "edad") @Nullable            private String edad;
+        @ColumnInfo(name = "sexo") @Nullable            private String sexo;
+        @ColumnInfo(name = "ubi_dist") @Nullable        private String ubi_dist;
+        @ColumnInfo(name = "ubi_prov") @Nullable        private String ubi_prov;
+        @ColumnInfo(name = "ubi_depa") @Nullable        private String ubi_depa;
 
-        public tb(@NonNull String dni, @NonNull String nombre, @NonNull String apellido) {
+        public tb(@NonNull String dni, @Nullable String cod_verificcion, @NonNull String nombre, @NonNull String ap_paterno, @NonNull String ap_materno, @Nullable String nacimiento, @Nullable String edad, @Nullable String sexo, @Nullable String ubi_dist, @Nullable String ubi_prov, @Nullable String ubi_depa) {
             this.dni = dni;
+            this.cod_verificcion = cod_verificcion;
             this.nombre = nombre;
-            this.apellido = apellido;
+            this.ap_paterno = ap_paterno;
+            this.ap_materno = ap_materno;
+            this.nacimiento = nacimiento;
+            this.edad = edad;
+            this.sexo = sexo;
+            this.ubi_dist = ubi_dist;
+            this.ubi_prov = ubi_prov;
+            this.ubi_depa = ubi_depa;
         }
 
-        @NonNull public String getDni() { return dni; }
+        @NonNull
+        public String getDni() { return dni; }
         public void setDni(@NonNull String dni) { this.dni = dni; }
+        @Nullable public String getCod_verificcion() { return cod_verificcion; }
+        public void setCod_verificcion(@Nullable String cod_verificcion) { this.cod_verificcion = cod_verificcion; }
         @NonNull public String getNombre() { return nombre; }
         public void setNombre(@NonNull String nombre) { this.nombre = nombre; }
-        @NonNull public String getApellido() { return apellido; }
-        public void setApellido(@NonNull String apellido) { this.apellido = apellido; }
+        @NonNull public String getAp_paterno() { return ap_paterno; }
+        public void setAp_paterno(@NonNull String ap_paterno) { this.ap_paterno = ap_paterno; }
+        @NonNull public String getAp_materno() { return ap_materno; }
+        public void setAp_materno(@NonNull String ap_materno) { this.ap_materno = ap_materno; }
+        @Nullable public String getNacimiento() { return nacimiento; }
+        public void setNacimiento(@Nullable String nacimiento) { this.nacimiento = nacimiento; }
+        @Nullable public String getEdad() { return edad; }
+        public void setEdad(@Nullable String edad) { this.edad = edad; }
+        @Nullable public String getSexo() { return sexo; }
+        public void setSexo(@Nullable String sexo) { this.sexo = sexo; }
+        @Nullable public String getUbi_dist() { return ubi_dist; }
+        public void setUbi_dist(@Nullable String ubi_dist) { this.ubi_dist = ubi_dist; }
+        @Nullable public String getUbi_prov() { return ubi_prov; }
+        public void setUbi_prov(@Nullable String ubi_prov) { this.ubi_prov = ubi_prov; }
+        @Nullable public String getUbi_depa() { return ubi_depa; }
+        public void setUbi_depa(@Nullable String ubi_depa) { this.ubi_depa = ubi_depa; }
     }
 
     @Dao
